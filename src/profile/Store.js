1e0ed19f99141e9b91b5c36318c5386c9dac2156
Medelouali <Mohammedelouali1999@gmail.com>
2021-03-07 07:49:43 -0800

    some previous comment

--- src/profile/Store.js
+++ src/profile/Store.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
 function Store({logo, title, jsx_details}) {
     const [show, setShow] = useState(false);
+    const ref=useRef(null);
 
     const handleShow=()=>{
         setShow(!show);
     };
 
+    useEffect(() => {
+        if(ref.current)ref.current.scrollIntoView({behavior: "smooth", block: "end"});
+    }, [show])
     return (
         <div className="store">
             <img onClick={handleShow} src={logo} alt=""/>
             <h4 onClick={handleShow} className={show ? "selected": ""}>{title}</h4>
-            <div  className="details">
+            <div ref={ref} className="details">
                 { show ? jsx_details: null}
             </div>
         </div>
