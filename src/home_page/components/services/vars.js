8731519c791ac7f7395b955072e19f6ee5629717
Medelouali <Mohammedelouali1999@gmail.com>
2021-04-13 09:00:04 -0700

    styling the store

--- src/home_page/components/services/vars.js
+++ src/home_page/components/services/vars.js
@@ -37,7 +37,7 @@ export const web_details=`
 export const whoAmI=<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-alt" className="svg-inline--fa fa-user-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#000" d="M256 288c79.5 0 144-64.5 144-144S335.5 0 256 0 112 64.5 112 144s64.5 144 144 144zm128 32h-55.1c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16H128C57.3 320 0 377.3 0 448v16c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48v-16c0-70.7-57.3-128-128-128z"></path></svg>;
 export const whoAmI_label="Who Am I!?";
 export const whoAmI_details=`
-    My name is El Ouali Med, I am a 21 years old software engineering student from Morocco. 
+    My name is El Ouali Med, I am a 22 years old software engineering student from Morocco. 
     You know this who am I question reminds me of the Who Am I movie yup, I love that movie. 
     Let me tell you more about myself using some words, I am the type of person who works smarter 
     to get what he's looking for, I believe in one thing and only one and that is solving REAL problems is 
