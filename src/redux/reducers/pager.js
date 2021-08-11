1adff9098e6abd33d2ef5fb3702a1631918cc609
Medelouali <Mohammedelouali1999@gmail.com>
2021-08-11 09:50:51 -0700

    changing the colors

--- src/redux/reducers/pager.js
+++ src/redux/reducers/pager.js
@@ -1,5 +1,5 @@
 //the default is home not store 
-const reducer=(state="home", action)=>{
+const reducer=(state="profile", action)=>{
     switch(action.type){
         case "Page":
             state=action.payload;
